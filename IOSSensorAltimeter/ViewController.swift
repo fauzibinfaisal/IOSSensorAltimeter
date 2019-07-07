e0d83ab624b6e5d7eddff483d9f3310f1ca22d06
Fauzi <fauzibinfaisal@gmail.com>
2019-07-07 21:11:46 +0700

    initial commit

--- IOSSensorAltimeter/ViewController.swift
+++ IOSSensorAltimeter/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// TODO 1 : import CoreMotion
+import CoreMotion
 
+class ViewController: UIViewController {
+    
+    //TODO 2 : Initialize CMAltimeter
+    let altimeter = CMAltimeter()
+    @IBOutlet weak var relatitveAltitudeLabel: UILabel!
+    @IBOutlet weak var pressureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startAction(_ sender: Any) {
+        //TODO 3 : Check sensor availibity
+        if CMAltimeter.isRelativeAltitudeAvailable(){
+            
+            // TODO 4 : Start altimeter and create what to do with the data
+            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main) { (data, error) in
+                print(data!)
+                
+                //altitude : meters, pressure: kilopascal -> hPa , multiple 10
+                
+                self.relatitveAltitudeLabel.text = String.init(format: "Altitude: %.2f M", (data?.relativeAltitude.floatValue)!)
+                self.pressureLabel.text = String.init(format: "Pressure: %.2f hPA", (data?.pressure.floatValue)!*10)
+            }
+        }
+    }
+    
+    @IBAction func stopAction(_ sender: Any) {
+        altimeter.stopRelativeAltitudeUpdates()
+    }
 }
 
