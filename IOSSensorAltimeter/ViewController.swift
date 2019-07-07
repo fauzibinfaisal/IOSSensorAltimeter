d6fce75f56e7ed4ec3b58cc33531f8f692c22c15
Fauzi <fauzibinfaisal@gmail.com>
2019-07-07 10:03:39 +0700

    Initial Commit

---
+++ IOSSensorAltimeter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSSensorAltimeter
+//
+//  Created by Fauzi Fauzi on 07/07/19.
+//  Copyright © 2019 Fauzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
